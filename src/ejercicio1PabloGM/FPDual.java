b423f806d82f86bc6e49d93cd79b034532384338
fox1210 <pablogomezmelendez@gmail.com>
2020-05-23 18:30:33 +0200

    desarollo de programdor y clequeo de numero de empleados

--- src/ejercicio1PabloGM/FPDual.java
+++ src/ejercicio1PabloGM/FPDual.java
@@ -5,27 +5,38 @@ public class FPDual {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		testProgramador();
 		testEmpleado();
 
 	}// fin main
 
+	private static void testProgramador() {
+		System.out.println(Empleado.getNumeroEmpleados());
+		Programador pablo = new Programador("Pablo", "producion", "java");
+		System.out.println(Empleado.getNumeroEmpleados());
+	}//fin test programador
+
 	private static void testEmpleado() {
-		/*
-		 * //test Empresa
-		 * System.out.println(Empleado.EMPRESA);
-		 * 
-		 * //test vacaciones 
-		 * Empleado paco=new Empleado("Paco");
-		 * System.out.println(paco.getVacaciones()); paco.vacaciones();
-		 * System.out.println(paco.getVacaciones()); paco.vacaciones();
-		 * System.out.println(paco.getVacaciones());
-		 * 
-		 * //test numero de empleados 
-		 * System.out.println(Empleado.getNumeroEmpleados());
-		 * //se debe dar un error y finalizar en 127 for (int i = 0; i < 128 ; i++) {
-		 * Empleado paco=new Empleado("Paco");
-		 * System.out.println(Empleado.getNumeroEmpleados()); }
-		 */
-	}// fin test empleado
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");// inicio o fin
+		// test Empresa
+		System.out.println(Empleado.EMPRESA);
+		System.out.println("*****************************************************************");// Separador
+		// test vacaciones
+		Empleado paco = new Empleado("Paco");
+		System.out.println(paco.getVacaciones());
+		paco.vacaciones();
+		System.out.println(paco.getVacaciones());
+		paco.vacaciones();
+		System.out.println(paco.getVacaciones());
+		System.out.println("*****************************************************************");// Separador
+		// test numero de empleados
+		System.out.println(Empleado.getNumeroEmpleados());
+		// se debe dar dos error y finalizar en 127
+		for (int i = 0; i < 128; i++) {
+			Empleado alvaro = new Empleado("Paco");
+			System.out.println(Empleado.getNumeroEmpleados());
+		}
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");// inicio o fin
+	}//fon test empleado
 
 }// fin class fpdual
--- src/ejercicio1PabloGM/Programador.java
+++ src/ejercicio1PabloGM/Programador.java
@@ -3,13 +3,53 @@ package ejercicio1PabloGM;
 public class Programador extends Empleado {
 
 	// DELARACIONVDE VARIABLES
-	// CONSTRUCTORES
-	public Programador(String nombre) {
-		super(nombre);
-		// TODO Auto-generated constructor stub
-	}
-	// METODOS GETTERS Y SETTERS
-	// To String
-	// METODOS ESPECIFICOS
+	private int numeroDeEmpleado;
+	private String departamento;
+	private String especialidad;
 
+	// CONSTRUCTORES
+	public Programador(String nombre, String departamento, String especialidad) {
+		super(nombre);
+		this.numeroDeEmpleado = generadorNEmpleado();
+		this.departamento = departamento;
+		this.especialidad = especialidad;
+	}
+
+	// METODOS GETTERS Y SETTERS
+	public int getNumeroDeEmpleado() {
+		return numeroDeEmpleado;
+	}
+
+	public void setNumeroDeEmpleado(int numeroDeEmpleado) {
+		this.numeroDeEmpleado = numeroDeEmpleado;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	// To String
+	@Override
+	public String toString() {
+		return "Programador [Nombre= " + super.getNombre() + ", numeroDeEmpleado= " + numeroDeEmpleado
+				+ ", departamento= " + departamento + ", especialidad= " + especialidad + "]";
+	}
+
+	// METODOS ESPECIFICOS
+	private int generadorNEmpleado() {
+		int numero = super.getNumeroEmpleados() + 1;
+		return numero;
+	}
 }

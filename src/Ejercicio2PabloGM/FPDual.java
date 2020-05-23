90fde1445e7b744717d881ede05a70ac1fd2e2fe
fox1210 <pablogomezmelendez@gmail.com>
2020-05-23 20:53:37 +0200

    Inicalizacion del ejercicio pinball

---
+++ src/Ejercicio2PabloGM/FPDual.java
@@ -0,0 +1,10 @@
+package Ejercicio2PabloGM;
+
+public class FPDual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Ejercicio2PabloGM/Painball.java
@@ -0,0 +1,9 @@
+package Ejercicio2PabloGM;
+
+import java.lang.Math;
+
+public class Painball {
+	private long bola = (long)(Math.random()*10000+1);
+	
+
+}//fin painBall
